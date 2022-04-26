1c6bd0b49703ac8b2ad4490a7bf2e3f4bf7fbe0d
Tim Sylvester <tim@wetdogweather.com>
2022-04-25 17:40:04 -0700

    Fix compiler warnings

--- ios/library/WhirlyGlobe-MaplyComponent/src/gestures/MaplyPinchDelegate.mm
+++ ios/library/WhirlyGlobe-MaplyComponent/src/gestures/MaplyPinchDelegate.mm
@@ -73,8 +73,8 @@ using namespace Maply;
 
             self.mapView->cancelAnimation();
             [[NSNotificationCenter defaultCenter] postNotificationName:kZoomGestureDelegateDidStart object:self.mapView->tag];
+            break;
         }
-			break;
 		case UIGestureRecognizerStateChanged:
         {
             Point3d curLoc = self.mapView->getLoc();
@@ -99,7 +99,7 @@ using namespace Maply;
                     (wrapView.frame.size.height/2.0) - screenOffset.y());
                 Point3d newCenterGeoPoint;
                 testMapView.pointOnPlaneFromScreen(newMapCenterPoint, &modelTrans, frameSizeScaled, &newLoc, true);
-                newLoc = self.mapView->coordAdapter->displayToLocal(newLoc);
+                newLoc = self.mapView->getCoordAdapter()->displayToLocal(newLoc);
                 newLoc.z() = newZ;
 
                 testMapView.setLoc(newLoc, false);
@@ -109,8 +109,8 @@ using namespace Maply;
                     self.mapView->setLoc(newCenter, true);
                 }
             }
+            break;
         }
-			break;
         case UIGestureRecognizerStateEnded:
             [[NSNotificationCenter defaultCenter] postNotificationName:kZoomGestureDelegateDidEnd object:self.mapView->tag];
             break;
